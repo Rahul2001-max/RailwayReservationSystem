91a3f8209a9dae559d61f84755742ca36e73e82b
Rahul2001-max <rahul.khatikamare@igtsolutions.com>
2025-03-08 12:11:24 +0530

    Delete UserService directory

--- UserService/java/com/example/UserService/Service/UserServices.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserService.Service;
-import com.example.UserService.Entity.User;
-import java.util.List;
-
-public interface UserServices {
-    User createUser(User user);
-    User getUserById(Long id);
-    List<User> getAllUsers();
-}
